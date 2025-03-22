2a08eb72a21eab890f79e4cd3dfa1ef7af32ce0b
dsazanov <astan12324@gmail.com>
2025-03-22 21:34:51 +0600

    add swagger

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 )
 
+// @title Explore-Base API documentation
+// @version 1.0.1
+// @host http://localhost:8080
+// @BasePath
 func main() {
 	router := app.App()
 	err := router.Run(":8080")
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,13 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
 func App() *gin.Engine {
 	router := gin.Default()
-
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	return router
 }
